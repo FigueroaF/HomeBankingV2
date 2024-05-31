e840d547f36540f31162095632a24960e44e3155
Facundo Figueroa <figueroaf1939w@gmail.com>
2024-05-31 19:32:59 -0300

    Agregue un nuevo cliente a mis datos de prueba

--- Models/DBInitializer.cs
+++ Models/DBInitializer.cs
@@ -11,6 +11,7 @@
                     new Client {FirstName="Eduardo",LastName="Mendoza",Email="edu@gmail.com",Password="123"},
                     new Client {FirstName="Facundo",LastName="Figueroa",Email="facu@gmail.com",Password ="123"},
                     new Client {FirstName="Maria",LastName="Lopez",Email="maria@gmail.com",Password="123"},
+                    new Client {FirstName="Pedro",LastName="Gomez",Email="pedro@gmail.com",Password="124"},
                 };
 
                 context.Clients.AddRange(clients);
